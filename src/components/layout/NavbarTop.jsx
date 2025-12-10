44ed90b6fcb14de8e0303e7d5f4aa0924be5a4ca
milytoh <milytohgold@gmail.com>
2025-12-10 06:09:51 -0800

    chore(deps): install typescript

--- src/components/layout/NavbarTop.jsx
+++ src/components/layout/NavbarTop.jsx
@@ -1,10 +1,13 @@
+import logo from "../../assets/images/logos/ahia-logo.png"
+
 
 const NavbarTop = () => {
     return (
-      <div className=" flex items-center bg-primary fixed top-0 left-0 w-full p-4 pb-2 justify-between z-30">
+      <div className=" flex items-center bg-primary fixed top-0 left-0 w-full p-4 pb-2 justify-between z-30 h-20">
         <h2 className="text-[#f6f7f8] text-lg font-bold leading-tight tracking-[-0.015em] flex-1 text-center pl-12">
-          Ahiamarket
+          <img src={logo} alt="" className="h-40 w-40"/>
         </h2>
+
         <div className="flex w-12 items-center justify-end gap-2">
           <button className="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-12 bg-transparent text-[#0c1d19] gap-2 text-base font-bold leading-normal tracking-[0.015em] min-w-0 p-0">
             <div
