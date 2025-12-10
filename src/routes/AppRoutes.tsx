018dc19cfbab82f7396ebd99fda82a3e6bc98584
milytoh <milytohgold@gmail.com>
2025-12-10 06:41:18 -0800

    refactor(ts): change from .jsx to .tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import {  Routes, Route } from "react-router-dom";
-import Layout from "../components/layout/layout";
-import Home from "../pages/home";
+import Layout from "../components/layout/Layout";
+import Home from "../pages/Home";
 
 const AppRoutes = () => {
     
