27f75a3c0f4bede2fa610163bce15f3b2344bf39
milytoh <milytohgold@gmail.com>
2025-12-12 05:30:44 -0800

    feat(scrow): add scroll top, on route navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import AppRoutes from './routes/AppRoutes';
 
+import useScrollToTop from "@/hooks/useScrollToTop";
 
 
 function App() {
+  useScrollToTop()
   return (
    
     <AppRoutes/>
