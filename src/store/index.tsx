f3559d9f3264b8901423c29c16081db9d318741e
milytoh <milytohgold@gmail.com>
2026-01-19 04:44:10 -0800

    chore(store hook): add store hook with expected types

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -8,6 +8,6 @@ export const store = configureStore({
   },
 });
 
-// 🔹 Types for TS
+// Types for TS
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
