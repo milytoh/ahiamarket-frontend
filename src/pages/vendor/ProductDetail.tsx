41f9324b8c3488b05fc7e0de7fadd20f08d55781
milytoh <milytohgold@gmail.com>
2026-05-14 06:35:56 -0700

    feat(Product): initializing vendor product detail

---
+++ src/pages/vendor/ProductDetail.tsx
@@ -0,0 +1,10 @@
+
+
+export default function ProductDetail() {
+
+    return (
+        <div className="bg-background-light min-h-screen p-6 md:p-10 max-w-7xl mx-auto">
+            
+         </div> 
+    )
+ }
